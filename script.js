543344098a4757942afd77a07717b182ad4757c8
jeromecox <jeromecox@hotmail.com>
2022-11-17 13:36:04 -0600

    fields are required, started cvv

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Add validation!');
+console.log("Add validation!");
+
+const parkForm = document.querySelector("#container");
+const submit = document.querySelector("#submit-button");
+const numDays = document.querySelector("#days");
+
+submit.addEventListener("click", function (event) {});
