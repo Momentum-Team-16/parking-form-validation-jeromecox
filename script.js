55097812e60c5860e8ab4457d5acba170cff0cfb
jeromecox <jeromecox@hotmail.com>
2022-11-18 14:55:27 -0600

    fixed infinite loop in problem 5, still not correct

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 console.log("Add validation!");
 
 const parkForm = document.querySelector("#container");
-let prices = [
-  7, 5, 5, 5, 5, 5, 7, 7, 5, 5, 5, 5, 5, 7, 7, 5, 5, 5, 5, 5, 7, 7, 5, 5, 5, 5,
-  5, 7, 7, 5, 5, 5, 5, 5, 7, 7, 5, 5, 5, 5, 5, 7, 7, 5, 5, 5, 5, 5, 7, 7,
-];
-
-// console.log(dayArray[3].day);
 
 // Solves problem 4 with constant price per day
 // parkForm.addEventListener("submit", function (event) {
@@ -22,52 +16,30 @@ let prices = [
 // });
 
 // Solves problem 5 with changing price for weekend
+let dayPrice = [7, 5, 5, 5, 5, 5, 7];
+
 parkForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   let dateDiv = document.querySelector("#start-date");
   let startDate = dateDiv.value;
   console.log(startDate);
-  let dayOfWeek = moment(startDate).format("dddd");
-  console.log(dayOfWeek);
+  let newDate = new Date(startDate);
+  console.log(`new date: ${newDate}`);
+  let dayOfWeek = newDate.getDay();
+  console.log(`index of day: ${dayOfWeek}`);
 
-  switch (dayOfWeek) {
-    case "Sunday":
-      priceIndex = 0;
-      break;
-    case "Monday":
-      priceIndex = 1;
-      break;
-    case "Tuesday":
-      priceIndex = 2;
-      break;
-    case "Wednesday":
-      priceIndex = 3;
-      break;
-    case "Thursday":
-      priceIndex = 4;
-      break;
-    case "Friday":
-      priceIndex = 5;
-      break;
-    case "Saturday":
-      priceIndex = 6;
-  }
+  console.log(dayPrice[dayOfWeek]);
 
-  console.log(`priceIndex: ${priceIndex}`);
+  let numDays = +document.querySelector("#days").value;
+  console.log(`number of days: ${numDays}`);
 
-  console.log(`price for that day: ${prices[priceIndex]}`);
-
-  let numDays = document.querySelector("#days");
-  console.log(`number of days: ${numDays.value}`);
+  let lastDay = dayOfWeek + numDays;
+  console.log(`index of last day + 1: ${lastDay}`);
 
   let totalText = 0;
-  for (
-    let i = priceIndex;
-    i < Number(priceIndex) + Number(numDays.value);
-    i++
-  ) {
-    totalText += prices[i];
+  for (i = dayOfWeek; i < lastDay; i++) {
+    totalText += dayPrice[dayOfWeek];
   }
   console.log(`totalText: ${totalText}`);
 
@@ -80,25 +52,25 @@ parkForm.addEventListener("submit", function (event) {
   let cardNum = document.querySelector("#credit-card").value;
   console.log(cardNum);
 
-  function validateCardNumber(number) {
-    var regex = new RegExp("^[0-9]{16}$");
-    if (!regex.test(cardNum)) return false;
+  // function validateCardNumber(number) {
+  //   var regex = new RegExp("^[0-9]{16}$");
+  //   if (!regex.test(cardNum)) return false;
 
-    return luhnCheck(cardNum);
-  }
+  //   return luhnCheck(cardNum);
+  // }
 
-  function luhnCheck(val) {
-    var sum = 0;
-    for (var i = 0; i < val.length; i++) {
-      var intVal = parseInt(val.substr(i, 1));
-      if (i % 2 == 0) {
-        intVal *= 2;
-        if (intVal > 9) {
-          intVal = 1 + (intVal % 10);
-        }
-      }
-      sum += intVal;
-    }
-    return sum % 10 == 0;
-  }
+  // function luhnCheck(val) {
+  //   var sum = 0;
+  //   for (var i = 0; i < val.length; i++) {
+  //     var intVal = parseInt(val.substr(i, 1));
+  //     if (i % 2 == 0) {
+  //       intVal *= 2;
+  //       if (intVal > 9) {
+  //         intVal = 1 + (intVal % 10);
+  //       }
+  //     }
+  //     sum += intVal;
+  //   }
+  //   return sum % 10 == 0;
+  // }
 });
